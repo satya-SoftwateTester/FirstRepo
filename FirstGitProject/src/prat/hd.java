9c018b1df65cbd5453a5839996eabe21ca671aa6
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-07-08 12:01:06 +0530

    Addition
    add two number

--- FirstGitProject/src/prat/hd.java
+++ FirstGitProject/src/prat/hd.java
@@ -1,10 +1,19 @@
 package prat;
 
+import java.util.Scanner;
+
 public class hd {
 
 	public static void main(String[] args) 
 	{
-System.out.println("sjajs");
+int a,b;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number");
+a=sc.nextInt();
+System.out.println("enter b number");
+b=sc.nextInt();
+int c=a+b;
+System.out.println(c);
 	}
 
 }
