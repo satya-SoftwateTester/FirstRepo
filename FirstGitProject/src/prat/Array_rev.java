0c106f6dbe8ff2e3b023dbc187938286ca7b8984
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-07-10 17:46:38 +0530

    create arr_rev
    program for array rev

---
+++ FirstGitProject/src/prat/Array_rev.java
@@ -0,0 +1,38 @@
+package prat;
+
+import java.util.Scanner;
+
+public class Array_rev {
+
+	public static void main(String[] args) 
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of an array");
+		n=sc.nextInt();
+		int a[]=new int[n];
+		System.out.println("Enter the element");
+		for(int i=0;i<n;i++) 
+		{
+			a[i]=sc.nextInt();
+
+		}
+		int size=n-1;
+		int temp=0;
+		for(int i=0;i<n/2;i++) 
+		{
+			temp=a[i];
+			a[i]=a[size];
+			a[size]=temp;
+
+			size--;
+
+		}
+		System.out.println("The reverse is ");
+		for (int i=0;i<n;i++) 
+		{
+			System.out.println(a[i]);
+		}
+	}
+
+}
