be0830f0047aa5ef5441d62a929d4f99697a3a98
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-07-10 15:13:08 +0530

    enter array element
    Array element enter

---
+++ FirstGitProject/src/prat/array_element_enter.java
@@ -0,0 +1,28 @@
+package prat;
+
+import java.util.Scanner;
+
+public class array_element_enter {
+
+	public static void main(String[] args) 
+	{
+		int n,temp;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the size of array");
+		n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Enter the element");
+		for(int i=0;i<n;i++) 
+		{
+			arr[i]=sc.nextInt();
+
+		}
+		for(int i=0;i<n;i++) 
+		{
+			System.out.println(arr[i]);
+			System.out.println(i);
+		}
+
+	}
+
+}
