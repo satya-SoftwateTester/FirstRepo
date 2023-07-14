12ac254d00a9adb655008878f5c238efe1211b58
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-07-14 15:53:02 +0530

    updated

--- FirstGitProject/src/java_basics/Patteren_1_123_1234.java
+++ FirstGitProject/src/java_basics/Patteren_1_123_1234.java
@@ -1,17 +1,20 @@
 package java_basics;
 
-public class Patteren1_22_333 {
+public class Patteren_1_123_1234 {
 
 	public static void main(String[] args) 
 	{
-		int n=5;
+		int n=4,temp=1;
 		for(int i=1;i<=n;i++) 
 		{
-			for( int j=1;j<=i;j++) 
+			for(int j=1;j<=i;j++) 
 			{
-				System.out.print(i);
+
+				System.out.print(temp);
+				temp++;
 			}
 			System.out.println("");
+
 		}
 	}
 
