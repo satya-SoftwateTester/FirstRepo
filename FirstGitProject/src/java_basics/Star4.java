16a7ded06b22f135cce8f1cbcaef71a782b8319e
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-07-17 10:57:07 +0530

    reverse right angle star triangle

---
+++ FirstGitProject/src/java_basics/Star4.java
@@ -0,0 +1,26 @@
+package java_basics;
+
+public class Star4 {
+
+	public static void main(String[] args)
+	{
+		int n=5;
+		for(int i=1;i<=n;i++) 
+		{
+			for(int j=1;j<i;j++) 
+			{
+				System.out.print(" ");
+
+			}
+			for(int k=0;k<=n-i;k++) 
+			{
+				System.out.print("*");
+
+			}
+
+			System.out.println(" ");
+
+		}
+	}
+
+}
