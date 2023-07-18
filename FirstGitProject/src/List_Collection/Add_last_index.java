f029ec7878ea82d60b319a1ed745499d2c172024
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-07-18 14:50:54 +0530

    collection 3

---
+++ FirstGitProject/src/List_Collection/Add_last_index.java
@@ -0,0 +1,17 @@
+package List_Collection;
+
+import java.util.LinkedList;
+
+public class Add_last_index {
+
+	public static void main(String[] args) 
+	{
+		LinkedList<Integer> l=new LinkedList<Integer>();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.addLast(40);
+		System.out.println(l);
+	}
+
+}
