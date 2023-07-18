213ada7fef757eb054e155575084277b135e2431
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-07-18 10:54:44 +0530

    array asecnding with exception

---
+++ FirstGitProject/src/excetion_handling/Array_asc.java
@@ -0,0 +1,59 @@
+package excetion_handling;
+
+import java.util.Scanner;
+
+public class Array_asc {
+
+	public static void main(String[] args) 
+	{
+		int n,temp;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of an array you want");
+		n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println(" the element are");
+		for(int i=0;i<n;i++) 
+		{
+			arr[i]=sc.nextInt();
+		}
+		try 
+		{
+			for(int i=0;i<n;i++) 
+			{
+				for(int j=i+1;j<n;j++) 
+				{
+					if(arr[i]>arr[j]) 
+					{
+						temp=arr[i];
+						arr[i]=arr[j+1];
+						arr[j]=temp;
+					}
+				}
+				System.out.println(arr[i]);
+			}
+
+		}
+		catch(ArithmeticException e) 
+		{
+			System.out.println("not good");
+		}
+		catch(NullPointerException m) 
+		{
+			System.out.println(" try again");
+		}
+		catch(IndexOutOfBoundsException q) 
+		{
+			System.out.println("correct the code");	
+		}
+		catch(Exception g) 
+		{
+			System.out.println("something went wrong");
+		}
+		finally 
+		{
+			System.out.println("memory released ");
+		}
+
+	}
+
+}
